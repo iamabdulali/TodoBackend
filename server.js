bad18bddf83b544923babdfdbe9cfca16934d9f8
Abdul Ali <abdulqayyum354@gmail.com>
2023-07-13 01:13:00 +0500

    commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import { app } from "./app.js";
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("API IS WORKING");
+});
+
 app.listen(process.env.PORT, () => {
   console.log(
     `server is working on ${process.env.PORT} in ${process.env.NODE_DEV} mode`
