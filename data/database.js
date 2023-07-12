9f4af8e9445f323db43e6120f68b100f378bf563
Abdul Ali <abdulqayyum354@gmail.com>
2023-07-13 01:04:17 +0500

    Engine Fixed

--- data/database.js
+++ data/database.js
@@ -1,11 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const connectDB = () =>{
-     mongoose.connect("mongodb://localhost:27017",{
-    dbName: "API"
-}).then(() =>{
-    console.log("DATABASE IS CONNECTED")
-}).catch(e =>{
-    console.log(e)
-}) 
-}
+export const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "API",
+    })
+    .then(() => {
+      console.log("DATABASE IS CONNECTED");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
